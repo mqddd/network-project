fea05083132ffc6f23798cc368c07f5fb50d9ce0
m.dehghan <meghdadd78@gmail.com>
2021-06-08 20:09:14 +0430

    first things first

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+const path = require('path');
+
 const express = require('express');
+
+const mainPage = require('./controller/main-controller');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(mainPage.mainPage);
 
 app.listen(3000);
---
+++ controller/main-controller.js
@@ -0,0 +1,6 @@
+exports.mainPage = (req, res, next) => {
+    res.render('main-page', {
+        pageTitle: 'Sudoku',
+        path: undefined
+    });
+};
